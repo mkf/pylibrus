ec0ae3fe07fd06811536d4f96a38f221b07f40b1
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-09-17 01:41:22 +0200

    Dodanie pożegnania na wieść o captcha

--- pylibrus.py
+++ pylibrus.py
@@ -67,7 +67,10 @@ assert br.viewing_html()
 print br.title()
 print odpoa.geturl()
 print odpoa.info() #d
-readzikdebugu = odpoa.read() 
+readzikdebugu = odpoa.read()
+if len(re.findall("Do zalogowania się wymagane jest dodatkowo wpisanie kodu z obrazka", readzikdebugu)):
+	print "Przerąbane. Że niby za dużo logowań, blablabla, CAPTCHA. Tego jeszcze nie obsługuję. Wychodzimy!"
+	quit() 
 if len(re.findall("Zaloguj się aby przejść do wybranej strony.", readzikdebugu)):
 	odpoah = restarcik(br,uzyszk,haselo)
 	assert br.viewing_html()
