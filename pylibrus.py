d6c270c5096647568ae98c907e4584ce0a976f9c
Michał Krzysztof Feiler <feiler.michal@gmail.com>
2014-09-18 21:00:40 +0200

    Stworzenie szkieletu dla nowych funkcjonalności oraz wpisanie w komentarzach paru pomysłów

--- pylibrus.py
+++ pylibrus.py
@@ -19,18 +19,37 @@ def czekanko(waiting):
 class corobienie:
     def __init__(self,br,polecenia):
         self.polecenia = polecenia
-        corobic = self.zdobadzpolecenie()
+        corobic = self.zdobadzpolecenie('Wpisz polecenie: ')
         if corobic == 'plan':
             self.plan(br, "n")
-        elif corobic == 'costam':
-            self.costam(br)
+        elif corobic == 'conowego':
+            self.conowego(br)
+        elif corobic == 'dzisiaj':
+            self.dzisiaj(br)
+        elif corobic == 'jutro':
+            self.jutro(br)
+        elif corobic == 'oceny':
+            self.oceny(br)
+        elif corobic == 'nieobec':
+            self.nieobec(br)
+        elif corobic == 'terminarz':
+            self.terminarz(br,"n")
+        #tutaj będzie elif że jeśli corobic to "terminarz YYYY-MM", to on wtedy odpala terminarz dla tego miesiaca, i/lub lepiej, może być terminarzwyb czy coś takiego, że będzie trochę interaktywny, podpolecenia tj. wybór daty w zasadzie wyłącznie, albo potem nawet od do hurtowe czytanie, co do tak wgl podpolecen wszystkich, to będzie trza dla każdego polecenia z podpoleceniami zrobic podpolecenie pomoc
+        elif corobic == 'ogloszenia':
+            self.ogloszenia(br)
+        elif corobic == 'szczesliwynr':
+            self.szczesliwynr(br)
+        elif corobic == 'pomoc':
+            print wytlumaczeniepolecen
+        elif corobic == 'wyjdz':
+            quit()
         else:
             print u'Błąd w kwestii informacji co robić'
             quit()
-    def zdobadzpolecenie(self):
+    def zdobadzpolecenie(self,pytanie):
         print 'Dostępne polecenia: '
         print polecenia
-        polec = raw_input('Wpisz polecenie: ')
+        polec = raw_input(pytanie)
         try:
             polecenia.index(polec)
             run = polec
@@ -41,9 +60,24 @@ class corobienie:
             return runu
     def plan(self,br,dajreada):
         szasowilasjkdfg = time.time()
-    def costam(self,br):
+    def conowego(self,br):
         czasik = time.time()
-polecenia = ('plan', 'costam')
+    def dzisiaj(self,br):
+        czasik = time.time()
+    def jutro(self,br):
+        czasik = time.time()
+    def oceny(self,br):
+        czasik = time.time()
+    def nieobec(self,br):
+        czasik = time.time()
+    def terminarz(self,br,wybtermcl):
+        czasik = time.time()
+    def ogloszenia(self,br):
+        czasik = time.time()
+    def szczesliwynr(self,br):
+        czasik = time.time()
+
+polecenia = ('plan', 'conowego', 'dzisiaj', 'jutro', 'oceny', 'nieobec', 'terminarz', 'ogloszenia', 'szczesliwynr', 'pomoc', 'wyjdz')
 br = mechanize.Browser()
 #br.open('https://dziennik.librus.pl/loguj/przenies/uczen_index')
 br.open('https://m.dziennik.librus.pl/module/Common/action/Login') #tymczasowo przestawiamy się na wersję mobilną — desktopowej chyba nie da się
@@ -93,5 +127,9 @@ except:
     #print odpoa.read()
     print readzikdebugu
 
+
+
 while True:
 	corobienie(br,polecenia)
+
+
